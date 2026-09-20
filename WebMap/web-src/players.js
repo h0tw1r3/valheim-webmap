f0762bad2ba15f956c7370aa1cc9cf32c1d6793a
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2026-09-20 17:12:50 +0000

    lint

--- WebMap/web-src/players.js
+++ WebMap/web-src/players.js
@@ -7,44 +7,48 @@ const playerMapIcons = {};
 let followingPlayer;
 
 const followPlayer = (playerMapIcon) => {
-    if (followingPlayer) {
-        followingPlayer.playerListEntry.el.classList.remove('selected');
-    }
-    if (playerMapIcon && playerMapIcon !== followingPlayer) {
-        followingPlayer = playerMapIcon;
-        followingPlayer.playerListEntry.el.classList.add('selected');
-        ui.map.classList.remove('smooth');
-        map.setFollowIcon(playerMapIcon);
-        ui.topMessage.textContent = `Following ${followingPlayer.name}`;
-        setTimeout(() => {
-            ui.map.classList.add('smooth');
-        }, 0);
-    } else {
-        followingPlayer = null;
-        map.setFollowIcon(null);
-        ui.map.classList.remove('smooth');
-        ui.topMessage.textContent = '';
-    }
+  if (followingPlayer) {
+    followingPlayer.playerListEntry.el.classList.remove("selected");
+  }
+  if (playerMapIcon && playerMapIcon !== followingPlayer) {
+    followingPlayer = playerMapIcon;
+    followingPlayer.playerListEntry.el.classList.add("selected");
+    ui.map.classList.remove("smooth");
+    map.setFollowIcon(playerMapIcon);
+    ui.topMessage.textContent = `Following ${followingPlayer.name}`;
+    setTimeout(() => {
+      ui.map.classList.add("smooth");
+    }, 0);
+  } else {
+    followingPlayer = null;
+    map.setFollowIcon(null);
+    ui.map.classList.remove("smooth");
+    ui.topMessage.textContent = "";
+  }
 };
 
 const init = () => {
-    websocket.addActionListener('players', (players) => {
-        let currentPlayerIds = Object.keys(playerMapIcons);
-        let newPlayerIds = players.map(player => { return player.id });
-        currentPlayerIds.filter(id => !newPlayerIds.includes(id)).forEach((id) => {
-            if (playerMapIcons[id] === followingPlayer) {
-                followPlayer(null);
-            }
-            playerMapIcons[id].playerListEntry.el.remove();
-            map.removeIcon(playerMapIcons[id]);
-            delete playerMapIcons[id];
-        });
+  websocket.addActionListener("players", (players) => {
+    let currentPlayerIds = Object.keys(playerMapIcons);
+    let newPlayerIds = players.map((player) => {
+      return player.id;
+    });
+    currentPlayerIds
+      .filter((id) => !newPlayerIds.includes(id))
+      .forEach((id) => {
+        if (playerMapIcons[id] === followingPlayer) {
+          followPlayer(null);
+        }
+        playerMapIcons[id].playerListEntry.el.remove();
+        map.removeIcon(playerMapIcons[id]);
+        delete playerMapIcons[id];
+      });
 
-        players.forEach((player) => {
-            let playerMapIcon = playerMapIcons[player.id];
-            if (!playerMapIcon) {
-                // new player
-                const playerListEntry = createUi(`
+    players.forEach((player) => {
+      let playerMapIcon = playerMapIcons[player.id];
+      if (!playerMapIcon) {
+        // new player
+        const playerListEntry = createUi(`
                     <div class="playerListEntry">
                         <div class="name" data-id="name"></div>
                         <div class="details" data-id="details">
@@ -55,63 +59,70 @@ const init = () => {
                         </div>
                     </div>
                 `);
-                playerListEntry.ui.name.textContent = player.name;
-                playerMapIcon = {
-                    ...player,
-                    type: 'player',
-                    text: player.name,
-                    zIndex: 5,
-                    playerListEntry
-                };
-                map.addIcon(playerMapIcon, false);
-                if (player.hidden && !constants.ALWAYS_VISIBLE) {
-                    playerListEntry.ui.details.style.display = 'none';
-                }
-                playerMapIcons[player.id] = playerMapIcon;
-                playerListEntry.el.addEventListener('click', () => {
-                    if (!playerMapIcon.hidden) {
-                        if (ui.playerListTut) {
-                            ui.playerListTut.remove();
-                            ui.playerListTut = undefined;
-                        }
-                        followPlayer(playerMapIcon);
-                    }
-                });
-
-                ui.playerList.appendChild(playerListEntry.el);
+        playerListEntry.ui.name.textContent = player.name;
+        playerMapIcon = {
+          ...player,
+          type: "player",
+          text: player.name,
+          zIndex: 5,
+          playerListEntry,
+        };
+        map.addIcon(playerMapIcon, false);
+        if (player.hidden && !constants.ALWAYS_VISIBLE) {
+          playerListEntry.ui.details.style.display = "none";
+        }
+        playerMapIcons[player.id] = playerMapIcon;
+        playerListEntry.el.addEventListener("click", () => {
+          if (!playerMapIcon.hidden) {
+            if (ui.playerListTut) {
+              ui.playerListTut.remove();
+              ui.playerListTut = undefined;
             }
-
-            if ((constants.ALWAYS_VISIBLE || !player.hidden) && playerMapIcon.hidden) {
-                // no longer hidden
-		        map.showIcon(playerMapIcon);
-                playerMapIcon.playerListEntry.ui.details.style.display = 'block';
-            } else if (!constants.ALWAYS_VISIBLE && (player.hidden && !playerMapIcon.hidden)) {
-                // becomming hidden
-                map.hideIcon(playerMapIcon);
-                playerMapIcon.playerListEntry.ui.details.style.display = 'none';
-                if (followingPlayer === playerMapIcon) {
-                    followPlayer(null);
-                }
-            }
-
-            playerMapIcon.lastUpdate = Date.now();
-            playerMapIcon.x = player.x;
-            playerMapIcon.z = player.z;
-            playerMapIcon.flags = player.flags;
-            console.log(player.flags);
-            console.log(playerMapIcon.flags);
-
-            playerMapIcon.playerListEntry.ui.hp.style.width = `${100 * Math.max(player.health / player.maxHealth, 0) }%`;
-            playerMapIcon.playerListEntry.ui.hpText.textContent = `${Math.round(Math.max(player.health, 0)) } / ${Math.round(player.maxHealth) }`;
-
-            if (!player.hidden || constants.ALWAYS_MAP || constants.ALWAYS_VISIBLE) {
-                map.explore(player.x, player.z);
-            }
+            followPlayer(playerMapIcon);
+          }
         });
-        map.updateIcons();
+
+        ui.playerList.appendChild(playerListEntry.el);
+      }
+
+      if (
+        (constants.ALWAYS_VISIBLE || !player.hidden) &&
+        playerMapIcon.hidden
+      ) {
+        // no longer hidden
+        map.showIcon(playerMapIcon);
+        playerMapIcon.playerListEntry.ui.details.style.display = "block";
+      } else if (
+        !constants.ALWAYS_VISIBLE &&
+        player.hidden &&
+        !playerMapIcon.hidden
+      ) {
+        // becomming hidden
+        map.hideIcon(playerMapIcon);
+        playerMapIcon.playerListEntry.ui.details.style.display = "none";
+        if (followingPlayer === playerMapIcon) {
+          followPlayer(null);
+        }
+      }
+
+      playerMapIcon.lastUpdate = Date.now();
+      playerMapIcon.x = player.x;
+      playerMapIcon.z = player.z;
+      playerMapIcon.flags = player.flags;
+      console.log(player.flags);
+      console.log(playerMapIcon.flags);
+
+      playerMapIcon.playerListEntry.ui.hp.style.width = `${100 * Math.max(player.health / player.maxHealth, 0)}%`;
+      playerMapIcon.playerListEntry.ui.hpText.textContent = `${Math.round(Math.max(player.health, 0))} / ${Math.round(player.maxHealth)}`;
+
+      if (!player.hidden || constants.ALWAYS_MAP || constants.ALWAYS_VISIBLE) {
+        map.explore(player.x, player.z);
+      }
     });
+    map.updateIcons();
+  });
 };
 
 export default {
-    init
+  init,
 };
--- WebMap/web-src/websocket.js
+++ WebMap/web-src/websocket.js
@@ -1,143 +1,142 @@
 const actionListeners = {};
 
 const addActionListener = (type, func) => {
-    const listeners = actionListeners[type] || [];
-    listeners.push(func);
-    actionListeners[type] = listeners;
+  const listeners = actionListeners[type] || [];
+  listeners.push(func);
+  actionListeners[type] = listeners;
 };
 
 const getActionListeners = (type) => {
-    return actionListeners[type] || [];
-}
-
-const actions = {
-    players: (lines, message) => {
-        const msg = message.replace(/^players\n/, '');
-        const playerSections = msg.split('\n\n');
-        const playerData = [];
-        playerSections.forEach(playerSection => {
-            const playerLines = playerSection.split('\n');
-            if (typeof playerLines[2] === 'undefined') {
-                return;
-            }
-            const newPlayer = {
-                id: playerLines.shift(),
-                name: playerLines.shift(),
-                health: playerLines.shift(),
-                maxHealth: playerLines.shift(),
-                flags: {
-                    dead: false,
-                    pvp: false,
-                    inbed: false,
-                },
-            };
-
-            if (playerLines[0] == 'hidden') {
-                newPlayer.hidden = true;
-                playerLines.shift();
-            } else {
-                newPlayer.hidden = false;
-            }
-            if (typeof playerLines[0] !== 'undefined') {
-                const xyz = playerLines.shift().split(',').map(parseFloat);
-                newPlayer.x = xyz[0];
-                newPlayer.z = xyz[1];
-            }
-            if (typeof playerLines[0] !== 'undefined') {
-                const flags = playerLines.shift();
-                const flag_types = ['dead', 'pvp', 'inbed'];
-                console.log(flags);
-                for (let i = 0; i < flag_types.length; i++) {
-                    console.log(i + " " + flag_types[i] + " " + flags[i]);
-                    newPlayer.flags[flag_types[i]] = Boolean(Number(flags[i]));
-                }
-            }
-            playerData.push(newPlayer);
-        });
-
-        actionListeners.players.forEach(func => {
-            func(playerData);
-        });
-    },
-    ping: (lines) => {
-        const xz = lines[2].split(',');
-        const ping = {
-            playerId: lines[0],
-            name: lines[1],
-            x: parseFloat(xz[0]),
-            z: parseFloat(xz[1])
-        };
-        actionListeners.ping.forEach(func => {
-            func(ping);
-        });
-    },
-    pin: (lines) => {
-        const xz = lines[4].split(',').map(parseFloat);
-        const pin = {
-            id: lines[1],
-            uid: lines[0],
-            type: lines[2],
-            name: lines[3],
-            x: xz[0],
-            z: xz[1],
-            text: lines[5]
-        };
-        actionListeners.pin.forEach(func => {
-            func(pin);
-        });
-    },
-    rmpin: (lines) => {
-        actionListeners.rmpin.forEach(func => {
-            func(lines[0]);
-        });
-    },
-    messages: (lines, message) => {
-        const msg = message.replace(/^messages\n/, '');
-        var messages = JSON.parse(msg);
-        actionListeners.messages.forEach(func => {
-            func(messages);
-        });
-    },
-    reload: (lines) => {
-	window.history.forward(1);
-    }
+  return actionListeners[type] || [];
 };
 
-Object.keys(actions).forEach(key => {
-    actionListeners[key] = [];
+const actions = {
+  players: (lines, message) => {
+    const msg = message.replace(/^players\n/, "");
+    const playerSections = msg.split("\n\n");
+    const playerData = [];
+    playerSections.forEach((playerSection) => {
+      const playerLines = playerSection.split("\n");
+      if (typeof playerLines[2] === "undefined") {
+        return;
+      }
+      const newPlayer = {
+        id: playerLines.shift(),
+        name: playerLines.shift(),
+        health: playerLines.shift(),
+        maxHealth: playerLines.shift(),
+        flags: {
+          dead: false,
+          pvp: false,
+          inbed: false,
+        },
+      };
+
+      if (playerLines[0] == "hidden") {
+        newPlayer.hidden = true;
+        playerLines.shift();
+      } else {
+        newPlayer.hidden = false;
+      }
+      if (typeof playerLines[0] !== "undefined") {
+        const xyz = playerLines.shift().split(",").map(parseFloat);
+        newPlayer.x = xyz[0];
+        newPlayer.z = xyz[1];
+      }
+      if (typeof playerLines[0] !== "undefined") {
+        const flags = playerLines.shift();
+        const flag_types = ["dead", "pvp", "inbed"];
+        console.log(flags);
+        for (let i = 0; i < flag_types.length; i++) {
+          console.log(i + " " + flag_types[i] + " " + flags[i]);
+          newPlayer.flags[flag_types[i]] = Boolean(Number(flags[i]));
+        }
+      }
+      playerData.push(newPlayer);
+    });
+
+    actionListeners.players.forEach((func) => {
+      func(playerData);
+    });
+  },
+  ping: (lines) => {
+    const xz = lines[2].split(",");
+    const ping = {
+      playerId: lines[0],
+      name: lines[1],
+      x: parseFloat(xz[0]),
+      z: parseFloat(xz[1]),
+    };
+    actionListeners.ping.forEach((func) => {
+      func(ping);
+    });
+  },
+  pin: (lines) => {
+    const xz = lines[4].split(",").map(parseFloat);
+    const pin = {
+      id: lines[1],
+      uid: lines[0],
+      type: lines[2],
+      name: lines[3],
+      x: xz[0],
+      z: xz[1],
+      text: lines[5],
+    };
+    actionListeners.pin.forEach((func) => {
+      func(pin);
+    });
+  },
+  rmpin: (lines) => {
+    actionListeners.rmpin.forEach((func) => {
+      func(lines[0]);
+    });
+  },
+  messages: (lines, message) => {
+    const msg = message.replace(/^messages\n/, "");
+    var messages = JSON.parse(msg);
+    actionListeners.messages.forEach((func) => {
+      func(messages);
+    });
+  },
+  reload: (lines) => {
+    window.history.forward(1);
+  },
+};
+
+Object.keys(actions).forEach((key) => {
+  actionListeners[key] = [];
 });
 
 let connectionTries = 0;
 const init = () => {
-    const websocketUrl = location.href.split('?')[0].replace(/^http/, 'ws');
-    const ws = new WebSocket(websocketUrl);
-    ws.addEventListener('message', (e) => {
-        const message = e.data.trim();
-        const lines = message.split('\n');
-        const action = lines.shift();
-        const actionFunc = actions[action];
-        if (actionFunc) {
-            actionFunc(lines, message);
-        } else {
-            console.log("unknown websocket message: ", e.data);
-        }
-    });
+  const websocketUrl = location.href.split("?")[0].replace(/^http/, "ws");
+  const ws = new WebSocket(websocketUrl);
+  ws.addEventListener("message", (e) => {
+    const message = e.data.trim();
+    const lines = message.split("\n");
+    const action = lines.shift();
+    const actionFunc = actions[action];
+    if (actionFunc) {
+      actionFunc(lines, message);
+    } else {
+      console.log("unknown websocket message: ", e.data);
+    }
+  });
 
-    ws.addEventListener('open', () => {
-        connectionTries = 0;
-        ws.send('players');
-    });
+  ws.addEventListener("open", () => {
+    connectionTries = 0;
+    ws.send("players");
+  });
 
-    ws.addEventListener('close', () => {
-        connectionTries++;
-        const seconds = Math.min(connectionTries * (connectionTries + 1), 120);
-        setTimeout(init, seconds * 1000);
-    });
+  ws.addEventListener("close", () => {
+    connectionTries++;
+    const seconds = Math.min(connectionTries * (connectionTries + 1), 120);
+    setTimeout(init, seconds * 1000);
+  });
 };
 
 export default {
-    init,
-    addActionListener,
-    getActionListeners
+  init,
+  addActionListener,
+  getActionListeners,
 };
-
