ef1338e587052567c5a1dcf6ab280af3373eaed8
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2026-09-23 02:16:25 +0000

    release 2.9.0

--- WebMap/WebMap.cs
+++ WebMap/WebMap.cs
@@ -27,7 +27,7 @@ namespace WebMap
     {
         public const string GUID = "com.github.h0tw1r3.valheim.webmap";
         public const string NAME = "WebMap";
-        public const string VERSION = "2.8.1";
+        public const string VERSION = "2.9.0";
 
         private static readonly string[] ALLOWED_PINS = { "dot", "fire", "mine", "house", "cave" };
 
